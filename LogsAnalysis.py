50337bb9d13e7d22498bd0b982e9fceb512c5c46
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-03-16 17:05:38 -0700

    Add select statement #1

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -3,3 +3,12 @@ import psycopg2
 DBNAME = "news"
 connection = psycopg2.connect(database=DBNAME)
 cursor = connection.cursor()
+
+try:
+    cursor.execute("""select articles.title, count(*) as views
+        from log, articles
+        where log.path like concat('%', articles.slug)
+        group by articles.title order by views desc limit 3""")
+except psycopg2.Error, e:
+    pass
+result = cursor.fetchall()
